067799c5d021028d78d8499aae717bfd6d6ca415
Zero5213 <Claymore_zero5213@mail.ru>
2022-08-16 12:17:45 +0300

    Убран том с конфигурацией nginx. Создана развертка базы из дампа при 1 запуске контейнера

--- PHP/src/index.php
+++ PHP/src/index.php
@@ -1,4 +1,12 @@
 <?php
-phpinfo();
-//mysqli_connect('mysql','root','root')
+
+
+$link = mysqli_connect('mysql','root','root');
+$res = mysqli_query($link,"SHOW DATABASES");
+
+while ($row = mysqli_fetch_assoc($res)) {
+    echo $row['Database'] . "\n";
+}
+
+
 ?>
